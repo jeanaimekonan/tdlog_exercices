8f9fe30e916c3112dc528fbf2630211fc4076c84
Ton Nom <ton.email@example.com>
2024-09-26 12:09:02 +0100

    Résolution de l'exercice 1

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -8,26 +8,45 @@ A new item can be created with Item(price, weight)
 Example of code using the class: i = Item(10, 20)
 """
 
-def trouver_nombres_pairs():
-   
-    p = []  
-    for i in range(101):  
-        if i % 2 == 0:  
-            p.append(i)  
-    return p  
 
-def trouver_multiples_de_7():
-   
-    m = [] 
-    for i in range(101):  
-        if i % 7 == 0:  
-            m.append(i)  
-    return m  
+class NombreUtils:
+    
+
+    def __init__(self):
+        
+        self.nombres_pairs = self.trouver_nombres_pairs()
+        self.multiples_de_7 = self.trouver_multiples_de_7()
+
+    def trouver_nombres_pairs(self):
+       
+        p = [] 
+        for i in range(101):  
+            if i % 2 == 0: 
+                p.append(i)  
+        return p  
+
+    def trouver_multiples_de_7(self):
+        
+        m = []  
+        for i in range(101):  
+            if i % 7 == 0:  
+                m.append(i)  
+        return m  
+    def __getitem__(self, index):
+        
+        if index == 0:
+            return self.nombres_pairs  # Retourner la liste des nombres pairs
+        elif index == 1:
+            return self.multiples_de_7  # Retourner la liste des multiples de 7
+        else:
+            raise IndexError("Index out of range. Use 0 for pairs and 1 for multiples of 7.")
 
 
-nombres_pairs = trouver_nombres_pairs()
-multiples_de_7 = trouver_multiples_de_7()
+nombre_utils = NombreUtils()
+
+
+nombres_pairs = nombre_utils[0]  
+multiples_de_7 = nombre_utils[1] 
 
 print("Nombres pairs entre 0 et 100 :", nombres_pairs)
 print("Multiples de 7 entre 0 et 100 :", multiples_de_7)
-
