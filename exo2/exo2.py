6964e0208881da9aa2d9e3f40ac06f38127e733b
Ton Nom <ton.email@example.com>
2024-09-27 08:58:32 +0100

    Résolution de l'exo2 par [Prénom Nom]

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,43 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+class NombrePremier:
+    def __init__(self):
+       
+        self.premiers = []
+
+    def est_premier(self, n):
+       
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    def generer_premiers(self, max_n):
+        
+        self.premiers = [num for num in range(2, max_n + 1) if self.est_premier(num)]
+        return self.premiers
+
+    def afficher_premiers(self):
+       
+        if not self.premiers:
+            print("Aucun nombre premier n'a été généré.")
+        else:
+            print(f"Nombres premiers générés : {self.premiers}")
+
+nombre_premier = NombrePremier()
+
+
+nombre_premier.generer_premiers(100)
+
+
+nombre_premier.afficher_premiers()
+
+
+nombre = 29
+if nombre_premier.est_premier(nombre):
+    print(f"{nombre} est un nombre premier.")
+else:
+    print(f"{nombre} n'est pas un nombre premier.")
